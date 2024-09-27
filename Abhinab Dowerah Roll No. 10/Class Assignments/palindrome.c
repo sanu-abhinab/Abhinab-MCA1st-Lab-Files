a9538e098beb7672e2eb4f1ec52b56295c2ef9a5
Abhinab Dowerah <118957612+sanu-abhinab@users.noreply.github.com>
2024-09-27 12:07:19 +0530

    palindrome.c

---
+++ Abhinab Dowerah Roll No. 10/Class Assignments/palindrome.c
@@ -0,0 +1,27 @@
+\\C Programe to check is a string is palindrome.
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+  char st[20], rst[20];
+  int i, j;
+  
+  printf("\n Enter the string: ");
+  scanf("%s",st);
+
+  i=0;
+  j=strlen(st) - 1;
+
+  while(j>=0)
+    {
+    rst[i] = st[j];
+    i++;
+    j--;
+    rst[i]='\0';
+
+    if(strcmp(st,rst) == 0)
+      printf("\n %s is a palindrome string", st);
+    else
+      printf("\n %s is not a palindrome string",st);
+  }
